2c8c1573ad3649e1d9f93bc52e0c57869de011a0
Vargm-b <brendavargasm.11student@gmail.com>
2026-04-09 23:55:38 -0400

    chore(core): configure express app with json middleware and api routes

--- backend/src/core/app.js
+++ backend/src/core/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
-const app  = express();
+const routes = require('./routes');
+
+const app = express();
 
 app.use(express.json());
+app.use('/api', routes);
+
 module.exports = app;
